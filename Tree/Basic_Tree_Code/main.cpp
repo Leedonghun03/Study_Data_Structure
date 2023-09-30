b3cfd1e7bdae09cfa3813c2adfdb64e96893d559
Hagom <51253792+Leedonghun03@users.noreply.github.com>
2023-09-30 22:17:02 +0900

    Add files via upload

---
+++ Tree/Basic_Tree_Code/main.cpp
@@ -0,0 +1,41 @@
+#include "Tree/basic_Tree.h"
+#include "Graph/graph.h"
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  tree m_tree;
+
+  {
+    m_tree.create_tree(3);
+  
+    m_tree.add_Node(3, 5);
+    m_tree.add_Node(3, 7);
+    m_tree.add_Node(3, 9);
+    cout << endl;
+  
+    m_tree.add_Node(5, 6);
+    m_tree.add_Node(5, 1);
+    cout << endl;
+  
+    m_tree.add_Node(7, 2);
+    m_tree.add_Node(7, 4);
+    cout << endl;
+  }
+
+  m_tree.preOrder(m_tree.root);
+  cout << endl;
+
+  m_tree.inOder(m_tree.root);
+  cout << endl;
+
+  m_tree.postOrder(m_tree.root);
+  cout << endl;
+
+  m_tree.levelOder(m_tree.root);
+  cout << endl;
+  
+  return 0;
+}
