5a2a8a43cf9dae8d4c2d453e2b166ce769866b2c
Hagom <51253792+Leedonghun03@users.noreply.github.com>
2023-10-01 23:50:40 +0900

    Update main.cpp

--- Tree/Binary_Serch_Tree/main.cpp
+++ Tree/Binary_Serch_Tree/main.cpp
@@ -9,16 +9,33 @@ int main()
   BST_TREE tree;
 
   tree.create_tree(8);
+  cout << endl;
 
-  tree.insert_Node(tree.root, 3);
-  tree.insert_Node(tree.root, 10);
-  tree.insert_Node(tree.root, 1);
-  tree.insert_Node(tree.root, 6);
-  tree.insert_Node(tree.root, 14);
-  tree.insert_Node(tree.root, 4);
-  tree.insert_Node(tree.root, 7);
-  tree.insert_Node(tree.root, 13);
+  {
+    tree.insert_Node(tree.root, 3);
+    tree.insert_Node(tree.root, 3);      //중복값 추가
+    tree.insert_Node(tree.root, 10);
+    tree.insert_Node(tree.root, 1);
+    tree.insert_Node(tree.root, 6);
+    tree.insert_Node(tree.root, 14);
+    tree.insert_Node(tree.root, 4);
+    tree.insert_Node(tree.root, 7);
+    tree.insert_Node(tree.root, 13);
+    cout << endl;
+  }
 
+  cout << "전위 출력: ";
+  tree.preOrder(tree.root);
+  cout << endl;
+
+  cout << "중위 출력: ";
+  tree.inOder(tree.root);
+  cout << endl;
+
+  cout << "후위 출력: ";
+  tree.postOrder(tree.root);
+  cout << endl;
+  
   tree.find_Node(tree.root, 3);
   
   return 0;
