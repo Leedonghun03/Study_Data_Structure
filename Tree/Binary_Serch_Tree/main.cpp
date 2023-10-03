c064aec2f43482b39bdf010efbef1e6ffefd8ba3
Hagom <51253792+Leedonghun03@users.noreply.github.com>
2023-10-03 14:59:07 +0900

    main.cpp

--- Tree/Binary_Serch_Tree/main.cpp
+++ Tree/Binary_Serch_Tree/main.cpp
@@ -6,37 +6,46 @@ using namespace std;
 int main() 
 {
   //BST_tree
-  BST_TREE tree;
-
-  tree.create_tree(8);
-  cout << endl;
+  BST_TREE BSTtree;
 
   {
-    tree.insert_Node(tree.root, 3);
-    tree.insert_Node(tree.root, 3);      //중복값 추가
-    tree.insert_Node(tree.root, 10);
-    tree.insert_Node(tree.root, 1);
-    tree.insert_Node(tree.root, 6);
-    tree.insert_Node(tree.root, 14);
-    tree.insert_Node(tree.root, 4);
-    tree.insert_Node(tree.root, 7);
-    tree.insert_Node(tree.root, 13);
+              //tree
+            //    8
+          //  3      10
+        //  1   6       14
+      //      4   7  13
+    
+    BSTtree.create_tree(8);
     cout << endl;
+    
+    BSTtree.insert_Node(BSTtree.tree, 3);
+    BSTtree.insert_Node(BSTtree.tree, 3);      //중복값 추가
+    BSTtree.insert_Node(BSTtree.tree, 10);
+    BSTtree.insert_Node(BSTtree.tree, 1);
+    BSTtree.insert_Node(BSTtree.tree, 6);
+    BSTtree.insert_Node(BSTtree.tree, 14);
+    BSTtree.insert_Node(BSTtree.tree, 4);
+    BSTtree.insert_Node(BSTtree.tree, 7);
+    BSTtree.insert_Node(BSTtree.tree, 13);
+    cout << endl;
+
+    BSTtree.delete_Node(BSTtree.tree, 3);
+    BSTtree.find_Node(BSTtree.tree, 3);
   }
 
   cout << "전위 출력: ";
-  tree.preOrder(tree.root);
+  BSTtree.preOrder(BSTtree.tree);
   cout << endl;
 
   cout << "중위 출력: ";
-  tree.inOder(tree.root);
+  BSTtree.inOder(BSTtree.tree);
   cout << endl;
 
   cout << "후위 출력: ";
-  tree.postOrder(tree.root);
+  BSTtree.postOrder(BSTtree.tree);
   cout << endl;
   
-  tree.find_Node(tree.root, 3);
+  BSTtree.find_Node(BSTtree.tree, 3);
   
   return 0;
 }
